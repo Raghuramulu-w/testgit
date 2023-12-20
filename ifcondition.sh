5ab26c37282cf822189dd61bd1a0794b8963b3cf
raghuramulu-w <raghu0711@gmail.com>
2023-12-20 11:47:10 +0530

    my bug fix

--- ifcondition.sh
+++ ifcondition.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 NUMBER=$1
-echo "enter number"
-if [$number -gt "20"]
+#echo "enter number"
+if [$number -gt 20];
 then 
    echo "number is greater than 20"
-   else
+ else
    echo "number is less than 20"
-
-   fi
+ fi
